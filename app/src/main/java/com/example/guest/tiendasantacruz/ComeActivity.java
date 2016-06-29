ec7d022af0d025a3a026d1b5bb5b791c14083fcf
john dow and scott holland <john@>
2016-06-29 16:29:10 -0700

    Add content to nested pages

--- app/src/main/java/com/example/guest/tiendasantacruz/ComeActivity.java
+++ app/src/main/java/com/example/guest/tiendasantacruz/ComeActivity.java
@@ -2,12 +2,37 @@ package com.example.guest.tiendasantacruz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ComeActivity extends AppCompatActivity {
+    private TextView nTextView;
+    private ListView mListView;
+    private String[] comeItems = {"Crackers", "Sugar", "Cupcakes", "Garlic", "Vinegar", "Vanilla", "Cinnamon", "Beans", "Canola Oil", "Mayonnaise", "Bread", "Beef", "Cereal", "Eggs", "Oatmeal", "Corn", "Peas", "Cabbage", "Apples", "Bananas", "Grapes", "Mustard", "Oranges"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_come);
+
+        mListView = (ListView) findViewById(R.id.listView4);
+        nTextView = (TextView) findViewById(R.id.textView4);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, comeItems);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String comeItem = ((TextView) view).getText().toString();
+                Toast.makeText(ComeActivity.this, comeItem, Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
 }
